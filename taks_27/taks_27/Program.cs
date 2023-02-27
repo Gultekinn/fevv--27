73892cdfaee7b6596f59ec49990b77d6b0174e56
unknown <azizovagultakin@gmail.com>
2023-02-27 03:19:06 +0300

    first commit

---
+++ taks_27/taks_27/Program.cs
@@ -0,0 +1,134 @@
+﻿
+
+// task 1-(verilmis ededin- veya + olmasi )
+
+//int number=-3;
+//if (number == 0)
+//{
+//    Console.WriteLine("ne - nede + dir");
+//        }
+//else if(number>0)
+//{
+//    Console.WriteLine("reqem musbettir");
+//}
+//else
+//{
+//    Console.WriteLine("reqem menfidir");
+//}
+
+
+//task 2-(Verilmish ededin tek ve ya cut olmasi)
+
+
+
+//int number = 5;
+//if (number % 2 == 0)
+//{
+//    Console.WriteLine("cut eddedir");
+//}
+//else if (number != 0)
+//{
+//    Console.WriteLine("tek ededdir");
+//}
+
+//else
+//{
+//    Console.WriteLine(" ne tek nede cut eddedir");
+
+//}
+
+
+
+
+
+
+
+//task 3 Dogum tarixi verilir uzerine 18 gelirsiz eger netice 2023den boyukdurse ekrana 18 yashiniz tamam olmayib, kicikdirse tamam olub 
+
+//int year = 2009;
+//if ((year += 18) < 2023)
+
+//{
+//    Console.WriteLine("18 tamam olmayib");
+//}
+//else if (year > 2023)
+//{
+//    Console.WriteLine("18 tamam olub ");
+
+//}
+//else
+//if (year == 2023) 
+
+//{
+//    Console.WriteLine("yasi tam 18 dir");
+//}
+
+
+
+
+//task 4 (A, B, C reqemleri verilib. 5-den boyuk olanlarin cemi)
+
+
+
+
+
+
+int A = 7;
+if ((sum += A) > 5)
+{
+    Console.WriteLine("A boyukdur 5 den");
+}
+int B = 3;
+if (B < 5)
+{
+    Console.WriteLine("B kicikdir 5 den");
+}
+    int C = 9;
+if ((sum += C) > 5)
+{ }
+    Console.WriteLine("C boyukdur 5 den");
+}
+int sum = (A+B)
+if(A += B)
+{
+    Console.WriteLine("16");
+}
+
+
+
+
+
+// task 5 Verilmish 3 mertebeli ededin 100-luk mertebesini tapin
+
+//int x = 458;
+//Console.WriteLine(x / 100);
+
+
+
+//elave
+// task 2 meselen 333 veya 334 yada 354 versek 
+//int number = 364;
+// if (number %111 == 0)
+//{
+//    Console.WriteLine("tum reqemleri eynidir");
+//}
+// else if ( number %111 == 1)
+//{
+//    Console.WriteLine(" tum reqemleri ferqli deyil ");
+//}
+//else
+//{
+//    Console.WriteLine(" tum reqemleri ferqlidir");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
